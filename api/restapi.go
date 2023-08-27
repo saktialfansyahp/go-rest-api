62ad76af41c6443b1b2ab76b8081419978e8cac8
saktialfansyahp <saktialfansyahp@gmail.com>
2023-08-28 01:43:11 +0700

    nt2

--- api/restapi.go
+++ api/restapi.go
@@ -4,7 +4,9 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"github.com/saktialfansyahp/go-rest-api/router"
+	"github.com/saktialfansyahp/go-rest-api/controllers/authcontroller"
+	"github.com/saktialfansyahp/go-rest-api/controllers/productcontroller"
+	"github.com/saktialfansyahp/go-rest-api/middleware"
 )
 
 var(
@@ -12,7 +14,25 @@ var(
 )
 
 func myRoute(r *gin.RouterGroup) {
-	router.DefineRoutes()
+	r.GET("home", func(ctx *gin.Context) {
+		ctx.String(http.StatusOK, "Home")
+	})
+	r.POST("login", func(ctx *gin.Context) {
+		authcontroller.Login(ctx.Writer, ctx.Request)
+	})
+	r.POST("register", func(ctx *gin.Context) {
+		authcontroller.Register(ctx.Writer, ctx.Request)
+	})
+	r.GET("logout", func(ctx *gin.Context) {
+		authcontroller.Logout(ctx.Writer, ctx.Request)
+	})
+	api := r.Group("api")
+	{
+		api.Use(middleware.GINMiddleware())
+		api.GET("product", func(ctx *gin.Context) {
+			productcontroller.Index(ctx.Writer, ctx.Request)
+		})
+	}
 }
 
 func init(){
