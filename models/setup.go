ef6c09a7484853eb899e0965824e922a3d8e1b41
saktialfansyahp <saktialfansyahp@gmail.com>
2023-09-13 22:57:16 +0700

    edit database

--- models/setup.go
+++ models/setup.go
@@ -10,7 +10,7 @@ import (
 var DB *gorm.DB
 
 func ConnectDatabase(){
-	dbURL := "postgres://default:************@ep-shrill-base-66245357.ap-southeast-1.postgres.vercel-storage.com:5432/verceldb"
+	dbURL := "postgres://default:xBTaUne1kOj2@ep-shrill-base-66245357.ap-southeast-1.postgres.vercel-storage.com:5432/verceldb"
 
     db, err := gorm.Open(postgres.Open(dbURL), &gorm.Config{})
 
