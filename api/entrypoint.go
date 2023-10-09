8e474693d35e46121012ca9ec2ceeacd1d612e92
saktialfansyahp <saktialfansyahp@gmail.com>
2023-10-09 17:24:46 +0700

    vercel deploy

--- api/entrypoint.go
+++ api/entrypoint.go
@@ -6,7 +6,7 @@ import (
 	"strings"
 
 	"github.com/gin-gonic/gin"
-	"github.com/saktialfansyahp/go-rest-api/controllers/authcontroller"
+	"github.com/saktialfansyahp/go-rest-api/controllers/productcontroller"
 	"github.com/saktialfansyahp/go-rest-api/models"
 )
 
@@ -14,67 +14,17 @@ var (
 	app *gin.Engine
 )
 
-func registerRouter(r *gin.RouterGroup) {
-
+func router(r *gin.RouterGroup) {
 	models.ConnectDatabase()
 
-	r.GET("/home", func(ctx *gin.Context) {
-		ctx.String(http.StatusOK, "Home")
+	r.GET("api/barang", func(ctx *gin.Context) {
+		productcontroller.Index(ctx.Writer, ctx.Request)
 	})
-	r.POST("/login", authcontroller.Login)
-	r.POST("/register", authcontroller.Register)
-	r.POST("/role", authcontroller.Role)
-	// r.GET("/logout", authcontroller.Logout)
-	// api := r.Group("api")
-	// api.GET("product", func(ctx *gin.Context) {
-	// 	productcontroller.Index(ctx.Writer, ctx.Request)
-	// })
-	// {
-	// 	admin := api.Group("admin")
-	// 	admin.Use(middleware.GINMiddleware("admin"))
-	// 	admin.GET("product/:id", func(ctx *gin.Context) {
-	// 		productcontroller.ById(ctx.Writer, ctx.Request, ctx.Param("id"))
-	// 	})
-	// 	admin.POST("product", func(ctx *gin.Context) {
-	// 		productcontroller.Create(ctx.Writer, ctx.Request)
-	// 	})
-	// 	admin.PUT("product/:id", func(ctx *gin.Context) {
-	// 		productcontroller.Edit(ctx.Writer, ctx.Request, ctx.Param("id"))
-	// 	})
-	// 	admin.DELETE("product/:id", func(ctx *gin.Context) {
-	// 		productcontroller.Delete(ctx.Writer, ctx.Request, ctx.Param("id"))
-	// 	})
-	// 	admin.GET("category", func(ctx *gin.Context) {
-	// 		categorycontroller.Index(ctx.Writer, ctx.Request)
-	// 	})
-	// 	admin.POST("category", func(ctx *gin.Context) {
-	// 		categorycontroller.Create(ctx.Writer, ctx.Request)
-	// 	})
-	// 	admin.GET("subcategory", func(ctx *gin.Context) {
-	// 		subcategorycontroller.Index(ctx.Writer, ctx.Request)
-	// 	})
-	// 	admin.POST("subcategory", func(ctx *gin.Context) {
-	// 		subcategorycontroller.Create(ctx.Writer, ctx.Request)
-	// 	})
-	// 	admin.GET("color", func(ctx *gin.Context) {
-	// 		colorcontroller.Index(ctx.Writer, ctx.Request)
-	// 	})
-	// 	admin.POST("color", func(ctx *gin.Context) {
-	// 		colorcontroller.Create(ctx.Writer, ctx.Request)
-	// 	})
-	// 	user := api.Group("user")
-	// 	user.Use(middleware.GINMiddleware("admin", "user"))
-	// 	user.GET("productUser", func(ctx *gin.Context) {
-	// 		productcontroller.User(ctx.Writer, ctx.Request)
-	// 	})
-	// }
 }
 
-// init gin app
-func init() {
+func init(){
 	app = gin.New()
 
-	// Handling routing errors
 	app.NoRoute(func(c *gin.Context) {
 		sb := &strings.Builder{}
 		sb.WriteString("routing err: no route, try this:\n")
@@ -110,11 +60,9 @@ func init() {
 
 	r := app.Group("/")
 
-	// register route
-	registerRouter(r)
+	router(r)
 }
 
-// entrypoint
 func Handler(w http.ResponseWriter, r *http.Request) {
 	app.ServeHTTP(w, r)
 }
